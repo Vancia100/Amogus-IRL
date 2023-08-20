982a103d3801fbd7b0e3bcef7cbb5fbe3961185e
Vancia100 <110672748+Vancia100@users.noreply.github.com>
2023-08-20 22:04:50 +0200

    Made something that at least looks somewhat like a website

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const app = express()
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const app = express();
 
-const indexRouter = require("./routes/index")
+const indexRouter = require("./routes/index");
 
-app.set("view engine", "ejs")
-app.set("views", __dirname + "/views")
-app.set ("layout", "layouts/layout")
-app.use(expressLayouts)
-app.use(express.static("public"))
+// Serve static files from the "public" directory
+app.use(express.static("public"));
 
-app.use("/", indexRouter)
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
 
-app.listen(process.env.PORT || 3000)
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on port " + (process.env.PORT || 3000));
+});
