b5ec124bc7025fa445301acc73980255c274b49b
Vancia100 <vilhelm.bergqvist@gmail.com>
2023-08-21 12:55:24 +0200

    Another directory with link from host

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const expressLayouts = require("express-ejs-layouts");
 const app = express();
 
 const indexRouter = require("./routes/index");
+const hostRouter = require("./routes/host")
 
 // Serve static files from the "public" directory
 app.use(express.static("public"));
@@ -13,6 +14,8 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 
 app.use("/", indexRouter);
+app.use("/host", hostRouter)
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port " + (process.env.PORT || 3000));
