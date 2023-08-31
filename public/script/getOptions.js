94b4f1d1aee7c8dc5acf3c43ebc8336cd77579cd
Vancia100 <vilhelm.bergqvist@gmail.com>
2023-08-31 16:09:35 +0200

    Some new stuff that does not work

--- public/script/getOptions.js
+++ public/script/getOptions.js
@@ -2,8 +2,10 @@ async function requestOptions () {
     try {
         const response = await fetch("/host/get-options")
             if (!response.ok) throw new Error("FAIL");
-        console.log(await response.json())
     } catch (error) {
         
     }
+    const settingsList = document.getElementById("settingsMenue")
+    settingsList.classList.toggle("show")
+    console.log(settingsList.classList)
 }
