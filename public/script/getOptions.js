2127578344d66b8efd7049cc531fe86e0ae992d9
Vancia100 <110672748+Vancia100@users.noreply.github.com>
2023-09-05 23:21:11 +0200

    Wow so much great changes
    API now works, with buttons styles and animations. Settings is stored in enabledTasks.json, is not planing to change that any time soon...

--- public/script/getOptions.js
+++ public/script/getOptions.js
@@ -1,38 +1,67 @@
 async function requestOptions () {
     const settingsList = document.getElementById("settingsMenue")
-    const ul = document.getElementById("settingslist")
+    const response = await sendAPI()
     if(!settingsList.classList.contains("show")) {
+        const ul = document.getElementById("settingslist")
         try {
-            const response = await sendAPI()
             //console.log(response)
-            for (let item of response) {
+            for (const item of response) {
                 const posibleItem = document.getElementById(item.name)
                 //console.log(posibleItem)
                 if(posibleItem) continue;
-                const addInString = '<add html ig? with process things?>'
+
                 const li = document.createElement("li")
-                li.setAttribute("id", item.name)
+                optionsDiv = document.createElement("div")
+                checkbox = document.createElement("input")
+                checkbox.setAttribute("type", "checkbox")
+                checkbox.setAttribute("id", item.name)
+                checkbox.checked  = item.enabled
+                textHeader = document.createElement("h3")
+                textHeader.classList.toggle("text1")
+                textHeader.innerHTML = (`Enable "${item.name}"`)
+                optionsDiv.classList.toggle("enableOption")
+                optionsDiv.appendChild(textHeader)
+                optionsDiv.appendChild(checkbox)
+                li.appendChild(optionsDiv)
                 ul.appendChild(li)
             }
         } catch (error) {
             console.log(error)
         }
     } else {
-        /*
-        find the settings and save it as a variable?
-        how do we make it send this on when a new site is loaded? save as a json that will be loaded by qr-code script?
-        Do we do this every time a button is pressed?
-        more advanced or use more process power?
-
-       current plan: send a json with the tasks enabled and dissabled to server that there gets saved as a .json files to presist betwean loads.
-       This api also sends back the enabled/dissabled to make it reflected in the settings
-        */
+        changePreferences()
     }
     const qrBtn = document.getElementById("qrBtn")
     const startBtn = document.getElementById("startBtn")
     qrBtn.classList.toggle("hide")
     settingsList.classList.toggle("show")
     startBtn.classList.toggle("hide")
+}
+async function changePreferences() {
+    const response = await sendAPI()
+    let apicall = {}
+    for (const item of response) {
+        const posibleItem = document.getElementById(item.name)
+        if (!posibleItem) {
+            apicall[item.name] = item.enabled ? item.enabled : false
+            continue
+        }
+        else {
+            apicall[item.name] = posibleItem.checked
+        }
+    }
+    const optionsSet = await fetch("/host/set-options", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(apicall)
+    })
+    if (!optionsSet.ok)  {
+        console.error("something went wrong when sending your prefrences...")
+        return
+    }
+    //console.log(await optionsSet.json)
 }
 
 async function sendAPI() {
@@ -45,3 +74,7 @@ async function sendAPI() {
         console.log(error)
     }
 }
+
+window.addEventListener("beforeunload", (event) => {
+    changePreferences()
+} )
--- routes/host.js
+++ routes/host.js
@@ -1,6 +1,11 @@
+const { error } = require("console")
 const express = require("express")
 const router = express.Router() 
 const fs = require("fs")
+const bodyParser = require('body-parser');
+
+
+router.use(bodyParser.json())
 
 router.get("/", (req, res) => {
     res.render("host")
@@ -9,14 +14,25 @@ router.get("/", (req, res) => {
 router.get("/get-options", (req, res) =>{
     try {
         const directory = (__dirname + "\\..\\tasks\\")
+        const taskEnableJsonRaw = (fs.readFileSync(directory + "enabledTasks.json", {
+            encoding: "utf-8", flag: "r"
+        }))
+        try {
+            var taskEnableJson = JSON.parse(taskEnableJsonRaw)
+        } catch (error) {
+            var taskEnableJson = {}
+        }
+        //console.log(taskEnableJson)
         const taskdirectory = fs.readdirSync(directory).filter( (thing) => {
             return(fs.statSync(`${directory}/${thing}`).isDirectory());
         })
         let taskList = []
         for (let task of taskdirectory) {
             const taskDir = fs.readdirSync(directory + task).filter((thing) => thing.endsWith(".js"))[0]
-            const {options} = require(directory + task + "\\" + taskDir) 
-            taskList.push({"name":options.name, "type":options.type}) 
+            const {options} = require(directory + task + "\\" + taskDir)
+            const taskEnabled = taskEnableJson[options.name] ? taskEnableJson[options.name] : false
+            
+            taskList.push({"name":options.name, "type":options.type, "enabled":taskEnabled}) 
         }
         res.json(taskList)
         //console.log(taskList)
@@ -24,6 +40,18 @@ router.get("/get-options", (req, res) =>{
         res.status(500).json({"error":error})
         console.log(error)
     }
+})
+
+router.post("/set-options", (req, res) =>{
+    const settings = req.body;
+    //console.log(settings)
+    const directory = (__dirname + "\\..\\tasks\\");
+    fs.writeFileSync(directory + "enabledTasks.json", JSON.stringify(settings, null, 4), { flag: 'w+', encoding: "utf-8" }, err => {
+        if (err) console.error(err)
+        return res.status(500).json({error: "Failed to save settings to file"})
+    });
+    
+    res.json({message: "Settings saved"})
 })
 
 router.get("/game", (req, res) => {
