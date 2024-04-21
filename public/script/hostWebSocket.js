56f0759112fbb594183d7f4f87e4073b50e17192
Vancia100 <110672748+Vancia100@users.noreply.github.com>
2024-04-21 21:14:56 +0200

    Improved visuals, somewhat works
    Added some visuals. This includes: Player icon (non customizable), player counter, confirmation message on client side, and non-centered divs.
    
    Also included a template to a function, wich will start the game, once the underlying server side components are done.
    
    TODO: Redo the CSS so that it looks good, preferable also scale better with phone and other aspect ratios.

--- public/script/hostWebSocket.js
+++ public/script/hostWebSocket.js
@@ -1,3 +1,4 @@
+let playerCount = 0
 
 
 document.addEventListener("DOMContentLoaded", () =>{
@@ -12,21 +13,33 @@ document.addEventListener("DOMContentLoaded", () =>{
         console.log(messageJSON)
         switch (messageJSON.event) {
             case "join":
+                updatePlayerCount()
                 console.log("Tied adding box")
                 const container = document.getElementById("playerContainer")
                 const playerDiv = document.createElement("div")
                 const playerText = document.createElement("h2")
+                const playerIcon = document.createElement("img")
+                const playerIconDiv = document.createElement("div")
+                const playerTextDiv = document.createElement("div")
+                playerIconDiv.classList.add("playerIconDiv")
+                playerTextDiv.classList.add("playerTextDiv")
+                playerIcon.id = `icon-${messageJSON.username}`
+                playerIcon.src = "/pictures/playericon.svg"
+                playerIcon.classList.add("playerIcon")
+                playerIcon.width = "55"
                 playerText.textContent = messageJSON.username
                 playerText.classList.add("text1")
                 playerDiv.classList.add("player")
                 playerDiv.id = messageJSON.username
-                playerDiv.appendChild(playerText)
-                playerDiv.addEventListener("click", (thisPlayerDiv) =>{
-                    console.log("tried kicking player", thisPlayerDiv.srcElement.id)
+                playerIconDiv.appendChild(playerIcon)
+                playerTextDiv.appendChild(playerText)
+                playerDiv.appendChild(playerIconDiv)
+                playerDiv.appendChild(playerTextDiv)
+                playerDiv.addEventListener("click", () =>{
                     socket.send(JSON.stringify({
                         client:"HOST",
                         action: "kick",
-                        player: thisPlayerDiv.srcElement.id ? thisPlayerDiv.srcElement.id : thisPlayerDiv.srcElement.parentNode.id,
+                        player: playerDiv.id
                     }))
                 })
                 container.appendChild(playerDiv)
@@ -36,6 +49,7 @@ document.addEventListener("DOMContentLoaded", () =>{
                 console.log("Tried kicking player", messageJSON.player)
                 nowKickedPlayer = document.getElementById(messageJSON.player)
                 if (nowKickedPlayer) {
+                    updatePlayerCount(true)
                     nowKickedPlayer.remove()
                 }
                 break
@@ -49,4 +63,26 @@ document.addEventListener("DOMContentLoaded", () =>{
     socket.addEventListener("close", () =>{
         window.location = "/"
     })
+
+    // Add envent listener for start game
+    const startBtn = document.getElementById("startBtn")
+    startBtn.addEventListener("click", () =>{
+        if(playerCount >= 4){
+            //Start Game
+        } else{
+            //Outline
+        }
+    })
 })
+
+
+function updatePlayerCount(kick = false) {
+    const playerCounterDiv = document.getElementById("playerCount")
+    if (kick) {
+        playerCount --
+    } else {
+        playerCount ++
+    }
+    playerCounterDiv.textContent = playerCount
+    //when start button is available add so that us unblurred
+}
