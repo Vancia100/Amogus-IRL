ac30fa74e96c3a8dff07971321d4665b1c8a22b7
Vancia100 <110672748+Vancia100@users.noreply.github.com>
2024-07-21 12:41:22 +0200

    /task test dir
    You can go to /task to try out all of the tasks work in a in-game environment.

--- code_tools/redoTasks.js
+++ code_tools/redoTasks.js
@@ -2,14 +2,16 @@ const {getQRCodes} = require("./read_all_files")
 
 const {taskList} = getQRCodes(true)
 
-
-for (item in taskList) {
-    try {
-        taskList[item].redoPublic() 
-        taskList[item].redoQR() 
-        console.log("redid", item)
-    } catch (error) {
-        console.log(error)
-        console.error(`could not redo QR for ${item}`)
+function redoTasks(){
+    for (item in taskList) {
+        try {
+            taskList[item].redoPublic() 
+            taskList[item].redoQR() 
+            console.log("redid", item)
+        } catch (error) {
+            console.log(error)
+            console.error(`could not redo QR for ${item}`)
+        }
     }
 }
+module.exports = redoTasks
--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -1,15 +1,19 @@
 const express = require("express")
 const router = express.Router()
 const {loopTasks} = require("./../code_tools/read_all_files")
+const redoTasks = require("./../code_tools/redoTasks")
 const fs = require("fs")
 const taskLookup = {}
-loopTasks(item =>{
+const taskEnableJson = loopTasks(item =>{
     taskLookup[item.name] = `${__dirname}/../public/tasks/${item.name}/${item._internals.file}`
 })
-
-router.get("/", (req, res) =>{
-    res.send("test")
+const taskList = taskEnableJson.enabled
+console.log(taskList)
+router.get("/", async (req, res) =>{
+    await redoTasks()
+    res.render("testTask", {taskList})
 })
+
 router.get("/:id", (req, res) =>{
     try {
         if (taskLookup[req.params.id]) {
